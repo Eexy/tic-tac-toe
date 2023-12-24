c3aa91ebfe233873ebdec35369fe0ad88e2cc3f9
Boussif Isai <eexdhe@gmail.com>
2023-12-25 00:09:31 +0100

    refactor: rename main class

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -5,7 +5,7 @@ import org.example.saver.TextSaver;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class TicTacToe {
     public static void main(String[] args) {
         Player p1 = new Player(PlayerSymbol.CROSS, "Player 1");
         Player p2 = new Player(PlayerSymbol.CIRCLE, "Player 2");
